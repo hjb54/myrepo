b2a646c0cc464f37d4c1b966f6a4f3b3fd6b87dc
Hannah Brown <hjbrown7@outlook.com>
2025-02-27 21:52:08 -0600

    Creates pipeline directory, retrieves and renames SRR data as paired-end fastq files for use in wrapper.py

--- SRRdown.py
+++ SRRdown.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 
 # Function to parse command line arguments
 def check_arg(args=None):
-    parser = argparse.ArgumentParser(description="Generate the adjacency list of a de Bruijn graph from sequencing reads.")
+    parser = argparse.ArgumentParser(description="Download, convert to fastq, and rename SRR files.")
     parser.add_argument("-i", "--input", help="input file", required=True)
     parser.add_argument("-o", "--output", help="output file", required=True)
     return parser.parse_args(args)
@@ -15,7 +15,7 @@ arguments = check_arg(sys.argv[1:])
 infile = arguments.input
 outfile = arguments.output
 
-s1 = ""
+s1 = "" #empty str vars for the SRR names
 s2 = ""
 with open (infile, 'r') as file:
     data = file.readlines()
@@ -32,18 +32,17 @@ else:
 def getfile(srr):
     hold_download = ('wget https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc='+srr) #holds the download command
     os.system(hold_download) #runs the download command in the terminal
-    name = ('mv fastq?acc='+(srr)+' '+ srr) #apparently dont need to rename but I did anyway
+    name = ('mv fastq?acc='+(srr)+' '+ srr) # renaming file
     os.system(name)
     split = ('fasterq-dump --split-files '+ srr) #split the files, run in terminal
     os.system(split)
-    #return()
+    return()
 
 getfile(s1)
 getfile(s2)
-os.system('mv '+s1+'_1.fastq'+' SSR1_1.fastq') #renames processed .fastq files to work with wrapper.py
-os.system('mv '+s1+'_2.fastq'+' SSR1_2.fastq')
-os.system('mv '+s2+'_1.fastq'+' SSR2_1.fastq')
-os.system('mv '+s2+'_2.fastq'+' SSR2_2.fastq')
+os.system('mv' + s1 + '_1.fastq' + 'SSR1_1.fastq') # renaming with a simpler notation, prevents silly spelling errors and allows for quick recognition
+os.system('mv' + s1 + '_2.fastq' + 'SSR1_2.fastq')
+os.system('mv' + s2 + '_1.fastq' + 'SSR2_1.fastq')
+os.system('mv' + s2 + '_2.fastq' + 'SSR2_2.fastq')
 
-with open(outfile, 'w') as out:
-    out.write('work pls')
+print('SRRdown.py has run.\nFull fastq files are downloaded and directory has been created.\nContinue to wrapper.py.')
